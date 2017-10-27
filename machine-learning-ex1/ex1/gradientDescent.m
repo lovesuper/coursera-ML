441a620267a7be1bfcf9a646c008dad870b92d57
Anton <antononrails@gmail.com>
2017-10-27 12:12:09 +0300

    first ex

---
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -0,0 +1,10 @@
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+  m = length(y);
+  J_history = zeros(num_iters, 1);
+
+  for iter = 1:num_iters
+      delta = 1 / m * sum(X .* repmat((X * theta - y), 1, size(X, 2)));
+      theta = (theta' - alpha * delta)';
+      J_history(iter) = computeCost(X, y, theta);
+  end
+end
